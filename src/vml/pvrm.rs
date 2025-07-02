b7d45d6a89d708e87abfe4c47cd7d78f8d18fce0
Austin Riba <austin@m51.io>
2025-07-01 22:46:29 -0700

    sep and seps

--- src/vml/pvrm.rs
+++ src/vml/pvrm.rs
@@ -1164,3 +1164,133 @@ pub mod matrix_vec_products {
         }
     }
 }
+
+/// Separation and position-angle
+/// - SEPP      angular separation from p-vectors
+/// - SEPS      angular separation from spherical coordinates
+/// - PAP       position-angle from p-vectors
+/// - PAS       position-angle from spherical coordinates
+pub mod sep_position_angle {
+    use crate::{
+        Pvector,
+        vml::pvrm::{
+            sphere_cart_conv::s2c,
+            vec_ops::{pdp, pm, pxp},
+        },
+    };
+
+    ///  Angular separation between two p-vectors.
+    ///
+    ///  This function is part of the International Astronomical Union's
+    ///  SOFA (Standards of Fundamental Astronomy) software collection.
+    ///
+    ///  Status:  vector/matrix support function.
+    ///
+    ///  Given:
+    ///     a      double[3]    first p-vector (not necessarily unit length)
+    ///     b      double[3]    second p-vector (not necessarily unit length)
+    ///
+    ///  Returned (function value):
+    ///            double       angular separation (radians, always positive)
+    ///
+    ///  Notes:
+    ///
+    ///  1) If either vector is null, a zero result is returned.
+    ///
+    ///  2) The angular separation is most simply formulated in terms of
+    ///     scalar product.  However, this gives poor accuracy for angles
+    ///     near zero and pi.  The present algorithm uses both cross product
+    ///     and dot product, to deliver full accuracy whatever the size of
+    ///     the angle.
+    ///
+    ///  Called:
+    ///     iauPxp       vector product of two p-vectors
+    ///     iauPm        modulus of p-vector
+    ///     iauPdp       scalar product of two p-vectors
+    ///
+    ///  This revision:  2021 May 11
+    ///
+    ///  SOFA release 2023-10-11
+    ///
+    ///  Copyright (C) 2023 IAU SOFA Board.  See notes at end.
+    pub fn sepp(a: &Pvector, b: &Pvector) -> f64 {
+        // Sine of angle between the vectors, multiplied by the two moduli.
+        let axb = pxp(a, b);
+        let ss = pm(&axb);
+
+        // Cosine of the angle, multiplied by the two moduli.
+        let cs = pdp(a, b);
+
+        // The angle.
+        if ss != 0.0 || cs != 0.0 {
+            ss.atan2(cs)
+        } else {
+            0.0
+        }
+    }
+    pub use sepp as angular_separation_pvector;
+
+    ///  Angular separation between two sets of spherical coordinates.
+    ///
+    ///  This function is part of the International Astronomical Union's
+    ///  SOFA (Standards of Fundamental Astronomy) software collection.
+    ///
+    ///  Status:  vector/matrix support function.
+    ///
+    ///  Given:
+    ///     al     double       first longitude (radians)
+    ///     ap     double       first latitude (radians)
+    ///     bl     double       second longitude (radians)
+    ///     bp     double       second latitude (radians)
+    ///
+    ///  Returned (function value):
+    ///            double       angular separation (radians)
+    ///
+    ///  Called:
+    ///     iauS2c       spherical coordinates to unit vector
+    ///     iauSepp      angular separation between two p-vectors
+    ///
+    ///  This revision:  2021 May 11
+    ///
+    ///  SOFA release 2023-10-11
+    ///
+    ///  Copyright (C) 2023 IAU SOFA Board.  See notes at end.
+    pub fn seps(al: f64, ap: f64, bl: f64, bp: f64) -> f64 {
+        // Spherical to Cartesian.
+        let ac = s2c(al, ap);
+        let bc = s2c(bl, bp);
+
+        // Angle between the vectors.
+        sepp(&ac, &bc)
+    }
+    pub use seps as angular_separation_spherical;
+
+    #[allow(clippy::excessive_precision)]
+    #[cfg(test)]
+    mod tests {
+        use assert_approx_eq::assert_approx_eq;
+
+        use super::*;
+
+        /// t_sofa.c t_sepp
+        #[test]
+        fn test_sepp() {
+            let a = [1.0, 0.1, 0.2];
+            let b = [-3.0, 1e-3, 0.2];
+            let s = sepp(&a, &b);
+            assert_approx_eq!(s, 2.860391919024660768, 1e-12);
+        }
+
+        /// t_sofa.c t_seps
+        #[test]
+        fn test_seps() {
+            let al = 1.0;
+            let ap = 0.1;
+
+            let bl = 0.2;
+            let bp = -3.0;
+            let s = seps(al, ap, bl, bp);
+            assert_approx_eq!(s, 2.346722016996998842, 1e-14);
+        }
+    }
+}
